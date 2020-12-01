f5f8e4f9fdbc19af58ad347e7cba46f7f93f83ce
kpac567 <71659832+kpac567@users.noreply.github.com>
2020-11-30 21:28:32 -0500

    created table body

---
+++ src/components/TableBody.js
@@ -0,0 +1,36 @@
+import React, { useContext } from "react";
+import PersonsContext from "./personsContext";
+//npm package to format birth dates
+import dateFormat from 'dateformat';
+
+
+const TableBody = () => {
+    const persons = useContext(PersonsContext);
+
+return (
+    
+<tbody>
+{persons.map ((person, index) => {
+    return (
+        <tr key={index}>
+            <td> <img className="rounded-circle"src={person.picture.thumbnail} alt=""/> </td>
+            <td>{person.name.first} {person.name.last} </td>
+            <td>{person.email} </td>
+            <td>{person.phone} </td>
+            <td>{dateFormat(person.dob.date, "mediumDate")} </td>
+        </tr>
+    );
+})}
+
+</tbody>
+
+);
+}
+
+export default TableBody;
+
+
+
+
+
+
